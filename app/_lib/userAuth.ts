d75a8876987078a2f8a56071ff5ad6b82233c14f
Olatoyan <golatoyan@gmail.com>
2024-07-05 03:34:11 +0100

    fix

--- app/_lib/userAuth.ts
+++ app/_lib/userAuth.ts
@@ -171,6 +171,7 @@ export async function loginWithEmailAndPassword({
     httpOnly: true,
     sameSite: "lax",
     expires: new Date(Date.now() + 60 * 60 * 24 * 1000),
+    secure: process.env.NODE_ENV === "production",
   });
 
   const test = await getSession();
