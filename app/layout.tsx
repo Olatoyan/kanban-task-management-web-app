885a49e1f5225419096000b4a804a2b899a261ca
Olatoyan <golatoyan@gmail.com>
2025-04-06 22:00:57 +0100

    remove unused SpeedInsights dependency and related import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
@@ -35,7 +34,6 @@ export default function RootLayout({
               {children}
 
               <Analytics />
-              <SpeedInsights />
             </main>
           </BoardProvider>
         </ThemeProvider>
